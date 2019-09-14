e98feb4cdad78664c2bbd568be7f060d352153d7
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2019-09-14 12:58:15 +0300

    both input and output must be explicitly specified

--- demo.py
+++ demo.py
@@ -7,23 +7,31 @@ myprog = pyd.Command('./myprog')
 file_r = pathlib.Path('data') / 'input'
 file_w = pathlib.Path('data') / 'output'
 
-# To execute a command without retaining output,
+# To execute a command without binding stdin and stdout
 # the user must explicitly pipe to None
 print('1')
-myprog >> None
-
+None >> myprog >> None
+# alternatively, use pyd.run
 print('2')
-file_r >> myprog >> None
+pyd.run(myprog)
+
+# two alternate ways to specify input file
 print('3')
+file_r >> myprog >> None
+print('4')
 myprog << file_r >> None
 
-print('4')
-file_r >> myprog >> file_w
+# both ends defined
 print('5')
+file_r >> myprog >> file_w
+print('6')
 myprog << file_r >> file_w
 
-print('6')
-myprog | myprog >> None
+# piping through multiple executables
 print('7')
+None >> myprog | myprog >> None
+print('8')
+pyd.run(myprog | myprog)
+print('9')
 file_r >> myprog | myprog >> file_w
 
--- pypedream/pypedream.py
+++ pypedream/pypedream.py
@@ -1,3 +1,9 @@
+class Unfilled(object):
+    def __repr__(self):
+        return 'UNFILLED'
+UNFILLED = Unfilled()
+
+
 class Command(object):
     def __init__(self, commandline):
         if isinstance(commandline, Command):
@@ -7,25 +13,23 @@ class Command(object):
 
     def __rshift__(self, other):
         """self >> other"""
-        pp = PartialPipeline(None, self)
+        pp = PartialPipeline(commands=[self])
         return pp >> other
 
     def __lshift__(self, other):
         """self << other"""
-        pp = PartialPipeline(other, self)
+        pp = PartialPipeline(input=other, commands=[self])
         return pp
 
     def __rrshift__(self, other):
         """other >> self"""
-        pp = PartialPipeline(other, self)
+        pp = PartialPipeline(input=other, commands=[self])
         return pp
 
     def __or__(self, other):
         """self | other"""
-        print('in __or__', self, other)
-        other = Command(other)
-        pp = PartialPipeline(self, other)
-        return pp
+        pp = PartialPipeline(commands=[self])
+        return pp | other
 
     def __add__(self, other):
         pass
@@ -38,32 +42,59 @@ class Command(object):
 
 
 class PartialPipeline(object):
-    def __init__(self, input, command):
+    def __init__(self, input=UNFILLED, commands=UNFILLED, output=UNFILLED):
         self.input = input
-        self.command = command
-        self.output = None
+        self.commands = [] if commands == UNFILLED else commands
+        self.output = output
+        if all(x is not UNFILLED for x in (self.input, self.output)):
+            self._execute()
 
     def __rshift__(self, other):
-        self.output = other
+        """self >> other"""
+        assert self.output == UNFILLED
+        pp = PartialPipeline(self.input, self.commands, other)
+        return pp
+
+    def __rrshift__(self, other):
+        """other >> self"""
+        assert self.input == UNFILLED
+        pp = PartialPipeline(other, self.commands, self.output)
+        return pp
+
+    def __or__(self, other):
+        """self | other"""
+        if isinstance(other, PartialPipeline):
+            assert self.output == UNFILLED
+            assert other.input == UNFILLED
+            commands = self.commands + other.commands
+            pp = PartialPipeline(self.input, commands, other.output)
+        else:
+            other = Command(other)
+            commands = self.commands + [other]
+            pp = PartialPipeline(self.input, commands, self.output)
+        return pp
+
+    def __repr__(self):
+        return '{}[{}, {}, {}]'.format(
+            self.__class__.__name__,
+            self.input, self.commands, self.output)
+
+    def _execute(self):
         if self.input is not None:
             print('Read input "{}"'.format(self.input))
-        print('Execute "{}"'.format(self.command))
+        for command in self.commands:
+            print('Execute "{}"'.format(command))
         if self.output is None:
             # execute without retaining output
             print('without retaining output')
         else:
             print('Write into "{}"'.format(self.output))
 
-    def __rrshift__(self, other):
-        pass
 
-    def __or__(self, other):
-        """self | other"""
-        other = Command(other)
-        pp = PartialPipeline(self, other)
-        return pp
-
-    def __repr__(self):
-        return '{}[{}, {}, {}]'.format(
-            self.__class__.__name__,
-            self.input, self.command, self.output)
+def run(partial_pipeline):
+    if isinstance(partial_pipeline, Command):
+        partial_pipeline = PartialPipeline(commands=[partial_pipeline])
+    input = None if partial_pipeline.input == UNFILLED else partial_pipeline.input
+    output = None if partial_pipeline.output == UNFILLED else partial_pipeline.output
+    pp = PartialPipeline(input, partial_pipeline.commands, output)
+    return pp
