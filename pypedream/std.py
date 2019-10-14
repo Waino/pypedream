a5b04293b28697286db4579d9675da1e46009396
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2019-10-14 19:58:59 +0300

    WIP: standard executables for boilerplate reduction

---
+++ pypedream/std.py
@@ -0,0 +1,44 @@
+### shorter names
+from .pypedream import Command as C
+from .pypedream import Function as F
+
+
+### some standard executables
+amixer = C('amixer')
+aplay = C('aplay')
+apt = C('apt')
+apt_add_repository = C('apt-add-repository')
+apt_config = C('apt-config')
+apt_get = C('apt-get')
+aptitude = C('aptitude')
+apt_key = C('apt-key')
+aspell = C('aspell')
+at = C('at')
+atools = C('atools')
+autoconf = C('autoconf')
+automake = C('automake')
+awk = C('awk')
+
+base32 = C('base32')
+base64 = C('base64')
+bibtex = C('bibtex')
+bzcat = C('bzcat')
+bzdiff = C('bzdiff')
+bzegrep = C('bzegrep')
+bzfgrep = C('bzfgrep')
+bzgrep = C('bzgrep')
+bzip2 = C('bzip2')
+bzless = C('bzless')
+
+cal = C('cal')
+chgrp = C('chgrp')
+chmod = C('chmod')
+chown = C('chown')
+clang = C('clang')
+cmake = C('cmake')
+cmp_ = C('cmp')
+cp = C('cp')
+cpp = C('cpp')
+curl = C('curl')
+cut = C('cut')
+cython = C('cython')
